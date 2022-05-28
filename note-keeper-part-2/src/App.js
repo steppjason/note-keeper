6b9e4ccb69979666d2cacff07df29c7c7a666926
Jason Stepp <hi@jasonstepp.me>
2022-05-27 21:14:45 -0700

    Added part 2

--- note-keeper-part-2/src/App.js
+++ note-keeper-part-2/src/App.js
@@ -1,13 +1,15 @@
+import React from "react"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import Note from "./components/Note"
+import Notes from "./data/Notes"
 
 function App() {
   return (
 	<div className="app">
 		  <Header />
 		  <main>
-			  <Note title="Note Title" content="Note content"/>
+			  {Notes.map((note) => <Note key={note.id} title={note.title} content={note.content}/>)}
 		  </main>
 		  <Footer />  
 	</div>
